a2134a949960210e689305d7872289efdef9db53
nelson12# <aw.nelson@hotmail.com>
2013-07-18 19:11:40 -0400

    Declaring variables and commenting
    Declaring doctors working

---
+++ Expressions Industry.js
@@ -0,0 +1,5 @@
+//adam nelson 071813 expressions industry
+//How many shots must be given per hour to get all the vaccines given for the day?
+var doctorNames=[ "Dr Flinstone", "Dr Rubble,"];//decclaring doctors;
+console.log(doctorNames);
+var doctorsWorking=prompt(" I am trying to determine the number of shots that are needed in an hour to get all the vaccines given for the day please enter number of doctors working");//declaring doctorsWorking and prompting for an answer;
