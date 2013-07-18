ce1c117523c90ce8b6c7cdd7d413422c8cfe72e7
nelson12# <aw.nelson@hotmail.com>
2013-07-18 19:58:31 -0400

    Final personal expression

---
+++ Expression personal.js
@@ -0,0 +1,11 @@
+//adam nelson 071813 expressions personal
+//How big is a sand box
+var sandIn= "sand";
+var square= "box";
+var sandBox= sandIn+" " +square;
+console.log(sandBox);
+var length=prompt("I am trying to calculate the volume of a sand box please enter length now");
+var width = prompt ("please enter width now");
+var depth= prompt("please neter depth now");
+var area=length*width*depth;
+alert("the area of the sand box is" +" "+" " +area+"square feet")
