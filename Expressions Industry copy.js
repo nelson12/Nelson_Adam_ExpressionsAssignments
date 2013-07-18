41decc6da244c8f8228aeea6fd17edd07893cf9a
nelson12# <aw.nelson@hotmail.com>
2013-07-18 19:23:11 -0400

    Declaring hours worked
    Declarting the hours doctors worked and adding comments

---
+++ Expressions Industry copy.js
@@ -0,0 +1,7 @@
+//adam nelson 071813 expressions industry
+//How many shots must be given per hour to get all the vaccines given for the day?
+var doctorNames=[ "Dr Flinstone", "Dr Rubble,"];//decclaring doctors;
+console.log(doctorNames);
+var doctorsWorking=prompt(" I am trying to determine the number of shots that are needed in an hour to get all the vaccines given for the day please enter number of doctors working");//declaring doctorsWorking and prompting for an answer;
+var hoursWorking=prompt("Please enter the total hours that the Doctors will be working");//declaring hours worked by both doctors
+
