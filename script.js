dadfc51cf5778d7acbcc0db4cdf9fefa47b415a5
nelson12# <aw.nelson@hotmail.com>
2013-07-18 17:04:39 -0400

    Concantenation

---
+++ script.js
@@ -0,0 +1,9 @@
+//adam nelson 071813 expressions_personal
+//how many bags of sand will it take to fill your sand box?
+var sandIn= "sand";
+var square= "box";
+var sandbox = sandIn+ ' '+square;
+console.log(sandbox);//this is the concatenation portin of the assignment
+
+
+
