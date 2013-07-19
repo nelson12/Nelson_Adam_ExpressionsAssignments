f727b9141b63f264296235a80a9624224e4fbffc
nelson12# <aw.nelson@hotmail.com>
2013-07-18 20:39:02 -0400

    Final Revision of Wacky Expression

---
+++ wacky expression revised.js
@@ -0,0 +1,10 @@
+//adam nelson 071813 expressions wacky
+//How many times will a guy get slapped in the face?
+var rude="pickup";
+var slap="lines";
+var results=rude+" "+slap;
+var cheesy=prompt("I am trying to see how many mistakes a guy can make before he is slapped in the face please enter the number of cheesy pickup lines till she would begin to be fed up");//declaring cheesy and prompting for a response
+var breast=prompt ("the number of breast pickup lines the same guy could use till the girl is now seeing a little more red");//declaring breast and promting for answer
+var buttGrab=prompt("The number of Butt grabs this same guy could get away with to this same girl before she...");//declaring buttGrab
+var results=(cheesy*2)+(breast*3)+(buttGrab*5)/2
+alert("Congrats you have been slapped in the face"+" "+results+" "+"times")
