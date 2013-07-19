1180473cfa538e1f11c16da1e4050c9e4ec3efec
nelson12# <aw.nelson@hotmail.com>
2013-07-18 20:14:23 -0400

    Start of wacky expressions assignment

---
+++ expressions wacky.js
@@ -0,0 +1,7 @@
+//adam nelson 071813 expressions wacky
+//How many times will a guy get slapped in the face?
+var rude="pickup";
+var slap="lines";
+var results=rude+" "+slap;//declaring variables
+var cheesy=prompt("I am trying to see how many mistakes a guy can make before he is slapped in the face please enter the number of cheesy pickup lines till she would begin to be fed up");//declaring cheesy and prompting for a response
+
